954ed847a508dbbc9e5ba9748d0f428fbbb29640
Michael Youniss <michaelyouniss@michaels-mbp.usma.ibm.com>
2016-08-10 15:37:25 -0400

    contact view added

--- src/js/public/bundle.js
+++ src/js/public/bundle.js
@@ -22022,6 +22022,10 @@
 	
 	var _MenuBar2 = _interopRequireDefault(_MenuBar);
 	
+	var _ContactView = __webpack_require__(/*! ./ContactView */ 181);
+	
+	var _ContactView2 = _interopRequireDefault(_ContactView);
+	
 	var _Constants = __webpack_require__(/*! ../../Constants */ 177);
 	
 	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
@@ -22066,11 +22070,7 @@
 	            ' PROJECTS '
 	          );
 	        case _Constants.Views.CONTACT_VIEW:
-	          return _react2.default.createElement(
-	            'div',
-	            null,
-	            ' CONTACT '
-	          );
+	          return _react2.default.createElement(_ContactView2.default, null);
 	        case _Constants.Views.RESUME_VIEW:
 	          return _react2.default.createElement(
 	            'div',
@@ -22433,6 +22433,69 @@
 	  item: _react2.default.PropTypes.object,
 	  handleItemClick: _react2.default.PropTypes.func
 	};
+
+/***/ },
+/* 181 */
+/*!******************************************!*\
+  !*** ./src/js/components/ContactView.js ***!
+  \******************************************/
+/***/ function(module, exports, __webpack_require__) {
+
+	'use strict';
+	
+	Object.defineProperty(exports, "__esModule", {
+	  value: true
+	});
+	
+	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+	
+	var _react = __webpack_require__(/*! react */ 1);
+	
+	var _react2 = _interopRequireDefault(_react);
+	
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+	
+	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+	
+	function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+	
+	function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+	
+	var ContactView = function (_React$Component) {
+	  _inherits(ContactView, _React$Component);
+	
+	  function ContactView() {
+	    _classCallCheck(this, ContactView);
+	
+	    return _possibleConstructorReturn(this, Object.getPrototypeOf(ContactView).apply(this, arguments));
+	  }
+	
+	  _createClass(ContactView, [{
+	    key: 'render',
+	    value: function render() {
+	
+	      return _react2.default.createElement(
+	        'div',
+	        null,
+	        _react2.default.createElement(
+	          'div',
+	          null,
+	          'michael.youniss13@gmail.com'
+	        ),
+	        _react2.default.createElement(
+	          'div',
+	          null,
+	          '(781) 801-5163'
+	        ),
+	        _react2.default.createElement('div', null)
+	      );
+	    }
+	  }]);
+	
+	  return ContactView;
+	}(_react2.default.Component);
+	
+	exports.default = ContactView;
 
 /***/ }
 /******/ ]);
