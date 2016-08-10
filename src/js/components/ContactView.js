46c4b3f8415c3f6234919f57cb7c7441a7d1a6ff
Michael Youniss <michaelyouniss@michaels-mbp.usma.ibm.com>
2016-08-10 17:06:00 -0400

    added contact page

--- src/js/components/ContactView.js
+++ src/js/components/ContactView.js
@@ -1,14 +1,18 @@
-import React from 'react'
+import React from 'react';
+
+import FaEnvelope from 'react-icons/lib/fa/envelope';
+import FaPhone from 'react-icons/lib/fa/phone';
+import GoMarkGithub from 'react-icons/lib/go/mark-github'
 
 export default class ContactView extends React.Component {
 
   render() {
 
     return (
-      <div>
-        <div>michael.youniss13@gmail.com</div>
-        <div>(781) 801-5163</div>
-        <div></div>
+      <div style={{ fontSize: '40px', textAlign: 'center', paddingTop: '25px'}}>
+        <div><FaEnvelope style={{ display: 'inline-block', paddingRight: '10px'}}/><div style={{ display: 'inline-block' }}>michael.youniss13@gmail.com</div></div>
+        <div><FaPhone style={{ display: 'inline-blcok', paddingRight: '10px'}} /><div style={{ display: 'inline-block' }}>(781) 801-5163</div></div>
+        <div><GoMarkGithub style={{ display: 'inline-block', paddingRight: '10px' }} /><div style={{ display: 'inline-block' }}>Michael-Youniss13</div></div>
       </div>
     )
   }
--- src/js/public/bundle.js
+++ src/js/public/bundle.js
@@ -22453,6 +22453,18 @@
 	
 	var _react2 = _interopRequireDefault(_react);
 	
+	var _envelope = __webpack_require__(/*! react-icons/lib/fa/envelope */ 183);
+	
+	var _envelope2 = _interopRequireDefault(_envelope);
+	
+	var _phone = __webpack_require__(/*! react-icons/lib/fa/phone */ 185);
+	
+	var _phone2 = _interopRequireDefault(_phone);
+	
+	var _markGithub = __webpack_require__(/*! react-icons/lib/go/mark-github */ 186);
+	
+	var _markGithub2 = _interopRequireDefault(_markGithub);
+	
 	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 	
 	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
@@ -22476,18 +22488,37 @@
 	
 	      return _react2.default.createElement(
 	        'div',
-	        null,
+	        { style: { fontSize: '40px', textAlign: 'center', paddingTop: '25px' } },
 	        _react2.default.createElement(
 	          'div',
 	          null,
-	          'michael.youniss13@gmail.com'
+	          _react2.default.createElement(_envelope2.default, { style: { display: 'inline-block', paddingRight: '10px' } }),
+	          _react2.default.createElement(
+	            'div',
+	            { style: { display: 'inline-block' } },
+	            'michael.youniss13@gmail.com'
+	          )
 	        ),
 	        _react2.default.createElement(
 	          'div',
 	          null,
-	          '(781) 801-5163'
+	          _react2.default.createElement(_phone2.default, { style: { display: 'inline-blcok', paddingRight: '10px' } }),
+	          _react2.default.createElement(
+	            'div',
+	            { style: { display: 'inline-block' } },
+	            '(781) 801-5163'
+	          )
 	        ),
-	        _react2.default.createElement('div', null)
+	        _react2.default.createElement(
+	          'div',
+	          null,
+	          _react2.default.createElement(_markGithub2.default, { style: { display: 'inline-block', paddingRight: '10px' } }),
+	          _react2.default.createElement(
+	            'div',
+	            { style: { display: 'inline-block' } },
+	            'Michael-Youniss13'
+	          )
+	        )
 	      );
 	    }
 	  }]);
@@ -22496,6 +22527,237 @@
 	}(_react2.default.Component);
 	
 	exports.default = ContactView;
+
+/***/ },
+/* 182 */,
+/* 183 */
+/*!******************************************!*\
+  !*** ./~/react-icons/lib/fa/envelope.js ***!
+  \******************************************/
+/***/ function(module, exports, __webpack_require__) {
+
+	'use strict';
+	
+	Object.defineProperty(exports, "__esModule", {
+	    value: true
+	});
+	
+	var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+	
+	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+	
+	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+	
+	function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+	
+	function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+	
+	var React = __webpack_require__(/*! react */ 1);
+	var IconBase = __webpack_require__(/*! react-icon-base */ 184);
+	
+	var FaEnvelope = function (_React$Component) {
+	    _inherits(FaEnvelope, _React$Component);
+	
+	    function FaEnvelope() {
+	        _classCallCheck(this, FaEnvelope);
+	
+	        return _possibleConstructorReturn(this, Object.getPrototypeOf(FaEnvelope).apply(this, arguments));
+	    }
+	
+	    _createClass(FaEnvelope, [{
+	        key: 'render',
+	        value: function render() {
+	            return React.createElement(
+	                IconBase,
+	                _extends({ viewBox: '0 0 40 40' }, this.props),
+	                React.createElement(
+	                    'g',
+	                    null,
+	                    React.createElement('path', { d: 'm40 15.8v17.8q0 1.4-1 2.5t-2.6 1h-32.8q-1.5 0-2.6-1t-1-2.5v-17.8q1 1.1 2.3 2 8 5.5 11 7.7 1.3 0.9 2.1 1.5t2.1 1 2.5 0.6h0q1.2 0 2.5-0.6t2.1-1 2.1-1.5q3.7-2.8 11.1-7.7 1.2-0.9 2.2-1.9z m0-6.5q0 1.7-1.1 3.4t-2.7 2.7q-8.4 5.8-10.5 7.3-0.2 0.1-0.9 0.6t-1.2 0.9-1.2 0.7-1.3 0.6-1.1 0.2h0q-0.5 0-1.1-0.2t-1.3-0.6-1.2-0.7-1.2-0.9-0.9-0.6q-2.1-1.5-5.9-4.1t-4.6-3.2q-1.3-0.9-2.6-2.6t-1.2-3q0-1.8 0.9-2.9t2.7-1.2h32.8q1.5 0 2.5 1.1t1.1 2.5z' })
+	                )
+	            );
+	        }
+	    }]);
+	
+	    return FaEnvelope;
+	}(React.Component);
+	
+	exports.default = FaEnvelope;
+	module.exports = exports['default'];
+
+/***/ },
+/* 184 */
+/*!******************************************************!*\
+  !*** ./~/react-icons/~/react-icon-base/lib/index.js ***!
+  \******************************************************/
+/***/ function(module, exports, __webpack_require__) {
+
+	'use strict';
+	
+	Object.defineProperty(exports, "__esModule", {
+	  value: true
+	});
+	
+	var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+	
+	var _react = __webpack_require__(/*! react */ 1);
+	
+	var _react2 = _interopRequireDefault(_react);
+	
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+	
+	function _objectWithoutProperties(obj, keys) { var target = {}; for (var i in obj) { if (keys.indexOf(i) >= 0) continue; if (!Object.prototype.hasOwnProperty.call(obj, i)) continue; target[i] = obj[i]; } return target; }
+	
+	var IconBase = function IconBase(_ref, _ref2) {
+	  var children = _ref.children;
+	  var color = _ref.color;
+	  var size = _ref.size;
+	  var style = _ref.style;
+	
+	  var props = _objectWithoutProperties(_ref, ['children', 'color', 'size', 'style']);
+	
+	  var _ref2$reactIconBase = _ref2.reactIconBase;
+	  var reactIconBase = _ref2$reactIconBase === undefined ? {} : _ref2$reactIconBase;
+	
+	  var computedSize = size || reactIconBase.size || '1em';
+	  return _react2.default.createElement('svg', _extends({
+	    children: children,
+	    fill: 'currentColor',
+	    preserveAspectRatio: 'xMidYMid meet',
+	    height: computedSize,
+	    width: computedSize
+	  }, reactIconBase, props, {
+	    style: _extends({
+	      verticalAlign: 'middle',
+	      color: color || reactIconBase.color
+	    }, reactIconBase.style || {}, style)
+	  }));
+	};
+	
+	IconBase.propTypes = {
+	  color: _react.PropTypes.string,
+	  size: _react.PropTypes.oneOfType([_react.PropTypes.string, _react.PropTypes.number]),
+	  style: _react.PropTypes.object
+	};
+	
+	IconBase.contextTypes = {
+	  reactIconBase: _react.PropTypes.shape(IconBase.propTypes)
+	};
+	
+	exports.default = IconBase;
+	module.exports = exports['default'];
+
+/***/ },
+/* 185 */
+/*!***************************************!*\
+  !*** ./~/react-icons/lib/fa/phone.js ***!
+  \***************************************/
+/***/ function(module, exports, __webpack_require__) {
+
+	'use strict';
+	
+	Object.defineProperty(exports, "__esModule", {
+	    value: true
+	});
+	
+	var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+	
+	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+	
+	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+	
+	function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+	
+	function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+	
+	var React = __webpack_require__(/*! react */ 1);
+	var IconBase = __webpack_require__(/*! react-icon-base */ 184);
+	
+	var FaPhone = function (_React$Component) {
+	    _inherits(FaPhone, _React$Component);
+	
+	    function FaPhone() {
+	        _classCallCheck(this, FaPhone);
+	
+	        return _possibleConstructorReturn(this, Object.getPrototypeOf(FaPhone).apply(this, arguments));
+	    }
+	
+	    _createClass(FaPhone, [{
+	        key: 'render',
+	        value: function render() {
+	            return React.createElement(
+	                IconBase,
+	                _extends({ viewBox: '0 0 40 40' }, this.props),
+	                React.createElement(
+	                    'g',
+	                    null,
+	                    React.createElement('path', { d: 'm35.9 27.7q0 0.6-0.2 1.6t-0.5 1.5q-0.4 1.1-2.7 2.3-2.1 1.2-4.1 1.2-0.6 0-1.2-0.1t-1.3-0.3-1.1-0.3-1.2-0.5-1.1-0.4q-2.2-0.7-3.9-1.8-2.8-1.8-5.9-4.8t-4.8-5.9q-1.1-1.7-1.9-3.9 0-0.2-0.4-1.1t-0.4-1.3-0.3-1-0.3-1.3-0.1-1.2q0-2 1.1-4.1 1.3-2.3 2.4-2.8 0.6-0.2 1.5-0.4t1.6-0.2q0.3 0 0.5 0 0.4 0.2 1.2 1.7 0.2 0.4 0.6 1.2t0.8 1.4 0.7 1.2q0.1 0.1 0.4 0.6t0.5 0.8 0.1 0.6q0 0.5-0.6 1.1t-1.4 1.3-1.4 1.1-0.6 1.1q0 0.2 0.1 0.5t0.2 0.4 0.3 0.6 0.3 0.4q1.7 3.1 3.8 5.2t5.3 3.9q0 0 0.4 0.3t0.6 0.3 0.4 0.2 0.5 0.1q0.4 0 1-0.6t1.2-1.4 1.3-1.4 1.1-0.6q0.3 0 0.6 0.1t0.8 0.5 0.6 0.4q0.5 0.3 1.1 0.7t1.5 0.8 1.2 0.6q1.5 0.8 1.7 1.2 0 0.2 0 0.5z' })
+	                )
+	            );
+	        }
+	    }]);
+	
+	    return FaPhone;
+	}(React.Component);
+	
+	exports.default = FaPhone;
+	module.exports = exports['default'];
+
+/***/ },
+/* 186 */
+/*!*********************************************!*\
+  !*** ./~/react-icons/lib/go/mark-github.js ***!
+  \*********************************************/
+/***/ function(module, exports, __webpack_require__) {
+
+	'use strict';
+	
+	Object.defineProperty(exports, "__esModule", {
+	    value: true
+	});
+	
+	var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+	
+	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+	
+	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+	
+	function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+	
+	function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+	
+	var React = __webpack_require__(/*! react */ 1);
+	var IconBase = __webpack_require__(/*! react-icon-base */ 184);
+	
+	var GoMarkGithub = function (_React$Component) {
+	    _inherits(GoMarkGithub, _React$Component);
+	
+	    function GoMarkGithub() {
+	        _classCallCheck(this, GoMarkGithub);
+	
+	        return _possibleConstructorReturn(this, Object.getPrototypeOf(GoMarkGithub).apply(this, arguments));
+	    }
+	
+	    _createClass(GoMarkGithub, [{
+	        key: 'render',
+	        value: function render() {
+	            return React.createElement(
+	                IconBase,
+	                _extends({ viewBox: '0 0 40 40' }, this.props),
+	                React.createElement(
+	                    'g',
+	                    null,
+	                    React.createElement('path', { d: 'm20 0c-11 0-20 9-20 20 0 8.8 5.7 16.3 13.7 19 1 0.2 1.3-0.5 1.3-1 0-0.5 0-2 0-3.7-5.5 1.2-6.7-2.4-6.7-2.4-0.9-2.3-2.2-2.9-2.2-2.9-1.9-1.2 0.1-1.2 0.1-1.2 2 0.1 3.1 2.1 3.1 2.1 1.7 3 4.6 2.1 5.8 1.6 0.2-1.3 0.7-2.2 1.3-2.7-4.5-0.5-9.2-2.2-9.2-9.8 0-2.2 0.8-4 2.1-5.4-0.2-0.5-0.9-2.6 0.2-5.3 0 0 1.7-0.5 5.5 2 1.6-0.4 3.3-0.6 5-0.6 1.7 0 3.4 0.2 5 0.7 3.8-2.6 5.5-2.1 5.5-2.1 1.1 2.8 0.4 4.8 0.2 5.3 1.3 1.4 2.1 3.2 2.1 5.4 0 7.6-4.7 9.3-9.2 9.8 0.7 0.6 1.4 1.9 1.4 3.7 0 2.7 0 4.9 0 5.5 0 0.6 0.3 1.2 1.3 1 8-2.7 13.7-10.2 13.7-19 0-11-9-20-20-20z' })
+	                )
+	            );
+	        }
+	    }]);
+	
+	    return GoMarkGithub;
+	}(React.Component);
+	
+	exports.default = GoMarkGithub;
+	module.exports = exports['default'];
 
 /***/ }
 /******/ ]);
