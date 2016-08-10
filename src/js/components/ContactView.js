86a38af10ad446683386db953cb05de6133134b8
Michael Youniss <michaelyouniss@michaels-mbp.usma.ibm.com>
2016-08-10 15:35:39 -0400

    compiled

---
+++ src/js/components/ContactView.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default class ContactView extends React.Component {
+
+  render() {
+
+    return (
+      <div>
+        <div>michael.youniss13@gmail.com</div>
+        <div>(781) 801-5163</div>
+        <div></div>
+      </div>
+    )
+  }
+}
